2788fde30cf4b0c122811cbcc49b00d642764304
Hamza EL OTMANI <142619220+Elotmanix@users.noreply.github.com>
2024-09-15 19:59:07 +0200

    Update phonenumber.py

--- problem_solving/phonenumber.py
+++ problem_solving/phonenumber.py
@@ -2,7 +2,6 @@
 import phonenumbers
 from phonenumbers import geocoder, carrier, timezone
 
-print("x" * 50)
 
 entered_num = input("Please enter a phone number: ")
 
@@ -12,13 +11,13 @@ phone_num = phonenumbers.parse(entered_num, None)
 
 print(phone_num)
 
-# You might want to get some information about the location that corresponds to a phone number.
+# information about the location that corresponds to a phone number.
 print(geocoder.description_for_number(phone_num, "ar"))
 
 # For mobile numbers in some countries,
-# you can also find out information about which carrier originally owned a phone number.
+# information about which carrier originally owned a phone number.
 print(carrier.name_for_number(phone_num, "en"))
 
 
-# You might also be able to retrieve a list of time zone names that the number potentially belongs to.
+# retrieve a list of time zone names that the number potentially belongs to.
 print(timezone.time_zones_for_number(phone_num))
